e7e180f8e57a92fff0ccba64999d8d93977f2a81
anas <mansouri.anas.98@gmail.com>
2026-08-19 01:42:05 +0200

    update testing tool

--- meta-anas-edge/recipes-apps/factory-stm32-check/files/main.cpp
+++ meta-anas-edge/recipes-apps/factory-stm32-check/files/main.cpp
@@ -31,112 +31,90 @@ int main()
     }
     sleep(3);
 
+    // handschake
     while (true)
     {
-        // handschake
-        while (true)
+        if (!uartManager.writeLine("PING"))
         {
-            if (!uartManager.writeLine("PING"))
-            {
-                log << "PI can' write" << std::endl
-                    << std::flush;
-                continue;
-            }
-            log << "PI sent PING" << std::endl
+            log << "PI can' write" << std::endl
                 << std::flush;
-            cc::utils::Result<std::string> response = uartManager.readLine();
-            if (!response)
-            {
-                log << "PI can' read" << std::endl
-                    << std::flush;
-                continue;
-            }
-            if (protocolParser.isAckandPing(response.unwrap()))
-            {
-                log << "PING received : " << std::endl;
-                break;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            continue;
         }
-
-    std::string cmd;
-    while (true)
-    {
-        cmd = "GET_STATUS";
-        cc::utils::Result<std::string> response;
-        {
-            std::lock_guard<std::mutex> lock(uartMutex);
-
-            if (!uartManager.writeLine(cmd))
-            {
-                log << "writing fail " << std::endl
-                    << std::flush;
-                break;
-            }
-            log << "PI Sent : " << cmd << std::endl
-                << std::flush;
-            response = uartManager.readLine();
-        }
+        log << "PI sent PING" << std::endl
+            << std::flush;
+        cc::utils::Result<std::string> response = uartManager.readLine();
         if (!response)
         {
-            log << "GET_STATUS read failed, going back to handshake" << std::endl
+            log << "PI can' read" << std::endl
                 << std::flush;
-            break;
+            continue;
         }
-        if (response)
+        if (protocolParser.isAckandPing(response.unwrap()))
         {
-            log << "STM32 REPLIES : " << response.unwrap() << std::endl
-                << std::flush;
-            if (protocolParser.isAck(response.unwrap()).unwrap())
-            {
-                log << "ack response" << std::endl
-                    << std::flush;
-            }
-            else if (protocolParser.isNAck(response.unwrap()).unwrap())
-            {
-                log << "Nack response" << std::endl
-                    << std::flush;
-            }
-            else if (protocolParser.isStatus(response.unwrap()).unwrap())
-            {
-                cc::utils::Result<cc::manager::Telemetry> data = protocolParser.parseStatus(response.unwrap());
-                if (data)
-                {
-                    std::string json =
-                        "{\"type\":\"machine_snapshot\","
-                        "\"temperature\":" +
-                        std::to_string(data.unwrap().temperature) +
-                        ",\"humidity\":" + std::to_string(data.unwrap().humidity) +
-                        ",\"load\":" + std::to_string(data.unwrap().load) +
-                        ",\"fan_rpm\":" + std::to_string(data.unwrap().fan_rpm) +
-                        ",\"vibration_x_mg\":" + std::to_string(data.unwrap().vibrationX_mg) +
-                        ",\"vibration_y_mg\":" + std::to_string(data.unwrap().vibrationY_mg) +
-                        ",\"vibration_z_mg\":" + std::to_string(data.unwrap().vibrationZ_mg) +
-                        ",\"vibration_level_mg\":" + std::to_string(data.unwrap().vibration_level_mg) +
-                        ",\"emergency_button\":" +
-                        std::string(data.unwrap().emergency_button ? "true" : "false") +
-                        ",\"state\":\"" +
-                        std::string(data.unwrap().machine_state) +
-                        "\",\"fault\":\"" +
-                        std::string(data.unwrap().fault) +
-                        "\",\"operating_mode\":\"" +
-                        std::string(data.unwrap().operating_mode) +
-                        "\",\"dht_status\":\"" +
-                        std::string(data.unwrap().dht_status) +
-                        "\",\"load_status\":\"" +
-                        std::string(data.unwrap().load_status) +
-                        "\"}\n";
-
-                    log<<json<<std::endl<<std::flush;
-                }
-                else
-                {
-                    log << "can't parse data" << std::endl
-                        << std::flush;
-                }
-            }
+            log << "PING received : " << std::endl;
+            break;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+
+    std::string cmd;
+    cmd = "GET_STATUS";
+    cc::utils::Result<std::string> response;
+    {
+        std::lock_guard<std::mutex> lock(uartMutex);
+
+        if (!uartManager.writeLine(cmd))
+        {
+            log << "writing fail " << std::endl
+                << std::flush;
+            return -1;
+        }
+        log << "PI Sent : " << cmd << std::endl
+            << std::flush;
+        response = uartManager.readLine();
+    }
+    if (!response)
+    {
+        log << "GET_STATUS read failed, going back to handshake" << std::endl
+            << std::flush;
+        return -1;
+    }
+    if (response)
+    {
+        log << "STM32 REPLIES : " << response.unwrap() << std::endl
+            << std::flush;
+
+        if (protocolParser.isStatus(response.unwrap()).unwrap())
+        {
+            cc::utils::Result<cc::manager::Telemetry> data = protocolParser.parseStatus(response.unwrap());
+            if (data)
+            {
+                if ((std::string(data.unwrap().dht_status) == "DHT_OK") &&
+                    (std::string(data.unwrap().load_status) == "LOAD_OK") &&
+                    (std::string(data.unwrap().operating_mode) == "AUTO_MODE") &&
+                    (std::string(data.unwrap().fault) == "FAULT_NONE"))
+                {
+                    log << "Test passed" << std::endl
+                        << std::flush;
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                log << "can't parse data" << std::endl
+                    << std::flush;
+                return -1;
+            }
+        }
+        else
+        {
+            log << "can't parse data" << std::endl
+                << std::flush;
+            return -1;
+        }
     }
 }
